23c4c015de999decd97e7cb54c54ac851aeac091
AishweryaK <aishweryakaushal01@gmail.com>
2024-06-24 18:21:58 +0530

    feat : ios_ad_done

---
+++ ios/InterstitialAdModule.h
@@ -0,0 +1,12 @@
+//
+//  InterstitialAdModule.h
+//  NoteTaking
+//
+//  Created by Alok Raj on 24/06/24.
+//
+
+#ifndef InterstitialAdModule_h
+#define InterstitialAdModule_h
+
+
+#endif /* InterstitialAdModule_h */
---
+++ ios/InterstitialAdModule.m
@@ -0,0 +1,8 @@
+//
+//  InterstitialAdModule.m
+//  NoteTaking
+//
+//  Created by Alok Raj on 24/06/24.
+//
+
+#import <Foundation/Foundation.h>
