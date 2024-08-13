3a031221f1d4bbcbda15c5fc0ce98099c889f773
AishweryaK <aishweryakaushal01@gmail.com>
2024-08-13 15:42:38 +0530

    fix: offline image handling

--- src/Screens/AddNoteScreen/ImageHandling.tsx
+++ src/Screens/AddNoteScreen/ImageHandling.tsx
@@ -1,31 +1,39 @@
-import React, { useEffect, useState } from 'react';
+import React, {useState} from 'react';
 import {TouchableOpacity} from 'react-native';
 import {ICONS} from '../../Constants/iconConstants';
 import ImageModal from '../../Components/ProfileImage/Modal';
-import { ProfileImageProps } from '../../Components/ProfileImage/profile_image';
+import {ProfileImageProps} from '../../Components/ProfileImage/profile_image';
 import useAuthentication from '../../Components/CustomHook/authHook';
-import { useReduxSelector } from '../../Redux/Store/store';
+import {useReduxSelector} from '../../Redux/Store/store';
+import {showAlert} from '../../Common/alert';
+import {ERR_MSG, ERR_TITLE} from '../../Constants/strings';
 
-function ImageHandling({onImageChange}:ProfileImageProps) {
-    const [modalVisible,setModalVisible] = useState<boolean>(false);
-    const {uid} = useReduxSelector(state=>state.user)
-    const {uploadImageToFirebaseNote} = useAuthentication();
+function ImageHandling({onImageChange}: ProfileImageProps) {
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
+  const {uid} = useReduxSelector(state => state.user);
+  const {uploadImageToFirebaseNote} = useAuthentication();
+  const connection = useReduxSelector(state => state.internet.connection);
 
   return (
     <>
-    <TouchableOpacity
-      onPress={() => {
-        setModalVisible(true);
-      }}>
-      {ICONS.CAM(24, 24, 'white')}
-    </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          if (connection) {
+            setModalVisible(true);
+          } else showAlert(ERR_TITLE.INTERNET, ERR_MSG.REQUEST_FAILED);
+        }}>
+        {ICONS.CAM(24, 24, 'white')}
+      </TouchableOpacity>
 
-    <ImageModal
+      <ImageModal
         modalVisible={modalVisible}
         setModalVisible={setModalVisible}
-        onImageChange={async(uri: string) => {
-            const downloadUri = await uploadImageToFirebaseNote({imageUri:uri,userId:uid});
-            onImageChange(downloadUri);
+        onImageChange={async (uri: string) => {
+          const downloadUri = await uploadImageToFirebaseNote({
+            imageUri: uri,
+            userId: uid,
+          });
+          onImageChange(downloadUri);
         }}
       />
     </>
